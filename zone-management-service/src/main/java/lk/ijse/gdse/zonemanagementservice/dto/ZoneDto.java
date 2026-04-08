6fb3a1de7ff21db71c0f02520ebbed56ee316eb3
chaminduchirantha <chaminduchirantha73@gmail.com>
2026-04-08 20:51:47 +0530

    create zone dto and zone entity in zone management system

--- zone-management-service/src/main/java/lk/ijse/gdse/zonemanagementservice/dto/ZoneDto.java
+++ zone-management-service/src/main/java/lk/ijse/gdse/zonemanagementservice/dto/ZoneDto.java
@@ -1,4 +1,21 @@
 package lk.ijse.gdse.zonemanagementservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ZoneDto {
+
+    private Long id;
+    private String name;
+    private Double minTemp;
+    private Double maxTemp;
+    private Double minHumidity;
+    private Double maxHumidity;
+    private String deviceId;
+    private String description;
+    private Long createdBy;
 }
--- zone-management-service/src/main/java/lk/ijse/gdse/zonemanagementservice/entitiy/ZoneEntity.java
+++ zone-management-service/src/main/java/lk/ijse/gdse/zonemanagementservice/entitiy/ZoneEntity.java
@@ -1,27 +1,42 @@
-package com.agms.zone_service.entity;
+package lk.ijse.gdse.zonemanagementservice.entitiy;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Getter
-@Setter
-@RequiredArgsConstructor
-@Table(name = "zones") // Database table name
+@Table(name = "zones")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ZoneEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private Double minTemp;
 
+    @Column(nullable = false)
     private Double maxTemp;
 
+    @Column(nullable = false)
+    private Double minHumidity;
+
+    @Column(nullable = false)
+    private Double maxHumidity;
+
+    @Column(nullable = true)
     private String deviceId;
 
+    @Column(nullable = true)
+    private String description;
+
+    @Column(nullable = false)
+    private Long createdBy;
 }
