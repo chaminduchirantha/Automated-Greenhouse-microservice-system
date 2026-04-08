a34e8b88be63cdb8de12bf50e4b2921e3780c300
chaminduchirantha <chaminduchirantha73@gmail.com>
2026-04-08 21:55:31 +0530

    ZoneManagementServiceApplication main file

--- zone-management-service/src/main/java/lk/ijse/gdse/zonemanagementservice/ZoneManagementServiceApplication.java
+++ zone-management-service/src/main/java/lk/ijse/gdse/zonemanagementservice/ZoneManagementServiceApplication.java
@@ -2,8 +2,12 @@ package lk.ijse.gdse.zonemanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ZoneManagementServiceApplication {
 
     public static void main(String[] args) {
