ebff6457c0fb0e0e24894dea35d923491bac66c6
chaminduchirantha <chaminduchirantha73@gmail.com>
2026-04-10 00:22:18 +0530

    main file crop inventory

--- crop-inventory-service/src/main/java/lk/ijse/gdse/cropinventoryservice/CropInventoryServiceApplication.java
+++ crop-inventory-service/src/main/java/lk/ijse/gdse/cropinventoryservice/CropInventoryServiceApplication.java
@@ -2,8 +2,10 @@ package lk.ijse.gdse.cropinventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CropInventoryServiceApplication {
 
     public static void main(String[] args) {
